130ff2ee305a851fe82def5a9c5d7c5ebbc1e87f
曹伟 <don_cao@sinobasedm.com>
2018-12-12 11:44:08 +0800

    修改

--- app/Http/Controllers/Web/MakeHtmlController.php
+++ app/Http/Controllers/Web/MakeHtmlController.php
@@ -10,12 +10,19 @@ namespace App\Http\Controllers\Web;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\View;
 
 class MakeHtmlController extends Controller
 {
-    public function index(){
-        $htmlContents = Response::view("webs.makes.index",['title'=>"hello","content"=>"hello world!"])->__toString();
-        var_dump($htmlContents);die;
+    public function index()
+    {
+        $fileSystem = new Filesystem();
+        $dir = config("filesystems.disks.public.root") . "/home";
+        $fileSystem->makeDirectory($dir);
+        $htmlContents = view("webs.makes.index", ['title' => "hello", "content" => "hello world!"])->__toString();
+        $fileSystem->replace($dir."test.html",$htmlContents);
+
     }
 }
